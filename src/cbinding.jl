4214c7e434bd0ab450ca22fa6c7607495848b7a8
Keith Rutkowski <keith@analytech-solutions.com>
2019-10-24 16:12:20 -0400

    Fix some @cbinding/@cextern behavior

--- src/cbinding.jl
+++ src/cbinding.jl
@@ -18,51 +18,52 @@ function _cbindings(mod::Module, exprs...)
 	
 	syms = []
 	libs = map(enumerate(exprs[1:end-1])) do (ind, expr)
-		sym = Symbol("lib_", ind)
+		sym = gensym("lib")
 		push!(syms, Expr(:$, sym))
-		return :($(sym) = $(esc(expr)))
+		return quote
+			$(sym) = $(esc(expr))
+		end
 	end
 	
 	block = Base.is_expr(exprs[end], :block) ? exprs[end] : Expr(:block, exprs[end])
 	for expr in block.args
-		if Base.is_expr(expr, :macrocall) && length(expr.args) > 1 && expr.args[1] in _externExprs
+		if Base.is_expr(expr, :macrocall) && length(expr.args) >= 1 && expr.args[1] in _externExprs
 			append!(expr.args, syms)
 		end
 	end
 	
 	return quote
 		$(libs...)
-		$(block)
+		@eval $(block)
 		nothing
 	end
 end
 
 
 
-macro cextern(exprs...) return _cextern(__module__, nothing, exprs...) end
+macro cextern(exprs...) return _cextern(__module__, exprs...) end
 
-function _cextern(mod::Module, deps::Union{Vector{Pair{Symbol, Expr}}, Nothing}, sig::Expr, libs::Union{Symbol, Expr}...)
-	return _cextern(mod, deps, nothing, sig, libs...)
+function _cextern(mod::Module, sig::Expr, libs::Union{Symbol, Expr, Clibrary}...)
+	return _cextern(mod, nothing, sig, libs...)
 end
 
-function _cextern(mod::Module, deps::Union{Vector{Pair{Symbol, Expr}}, Nothing}, conv::Union{Symbol, Nothing}, sig::Expr, libs::Union{Symbol, Expr}...)
+function _cextern(mod::Module, conv::Union{Symbol, Nothing}, sig::Expr, libs::Union{Symbol, Expr, Clibrary}...)
+	isempty(libs) && error("Usage of @cextern requires at least 1 library to be provided")
 	Base.is_expr(sig, :(::), 2) || error("Expected @cextern to have a function signature, `func(arg1::Arg1Type, ..., argN::ArgNType)::ReturnType`, or a global variable, `variable::VarType`, expression, but found `$(sig)`")
 	(externName, externType) = sig.args
-	return (externName isa Symbol ? _cextern_variable : _cextern_function)(mod, deps, conv, externName, externType, libs...)
+	return (externName isa Symbol ? _cextern_variable : _cextern_function)(mod, conv, externName, externType, libs...)
 end
 
-function _cextern_function(mod::Module, deps::Union{Vector{Pair{Symbol, Expr}}, Nothing}, conv::Union{Symbol, Nothing}, func::Expr, retType::Union{Symbol, Expr}, libs::Union{Symbol, Expr}...)
-	isnothing(deps) || error("Usage of @cextern cannot be nested")
-	deps = Pair{Symbol, Expr}[]
-	
+function _cextern_function(mod::Module, conv::Union{Symbol, Nothing}, func::Expr, retType::Union{Symbol, Expr}, libs::Union{Symbol, Expr, Clibrary}...)
 	isnothing(conv) || (startswith(String(conv), "__") && endswith(String(conv), "__") && length(String(conv)) > 4) || error("Expected @cextern to have a calling convention specified as `__CONVENTION__`, such as `__cdecl__` or `__stdcall__`, but got `$(conv)`")
+	conv = isnothing(conv) ? conv : Symbol(String(conv)[3:end-2])
 	
-	(Base.is_expr(func, :call) && length(func.args) > 1) || error("Expected @cextern to have a function `func(...)` in the function signature, but got `$(func)`")
+	(Base.is_expr(func, :call) && length(func.args) >= 1) || error("Expected @cextern to have a function `func(...)` in the function signature, but got `$(func)`")
 	func.args[1] isa Symbol || error("Expected @cextern to have a Symbol for a function name, but got `$(func)`")
 	name = func.args[1]
 	
 	argNames = []
-	args = map(func.args[2:end]) do arg
+	argTypes = map(func.args[2:end]) do arg
 		Base.is_expr(arg, :(::), 2) || Base.is_expr(arg, :(...), 1) || error("Expected @cextern to have function arguments specified as `argName::ArgType` or `argName...`, but got `$(arg)`")
 		if Base.is_expr(arg, :(::), 2)
 			(argName, argType) = arg.args
@@ -73,32 +74,29 @@ function _cextern_function(mod::Module, deps::Union{Vector{Pair{Symbol, Expr}}, 
 		return argType
 	end
 	
+	libs = map(l -> l isa Clibrary ? l : esc(l), libs)
 	sig = (
 		retType,
-		:(Tuple{$(args...)}),
-		(isnothing(conv) ? () : (:(CBinding.Cconvention{$(QuoteNode(Symbol(String(conv)[3:end-2])))}),))...,
+		:(Tuple{$(argTypes...)}),
+		(isnothing(conv) ? () : (:(CBinding.Cconvention{$(QuoteNode(conv))}),))...,
 	)
-	func = :(CBinding.Cfunction{$(sig...)}($(QuoteNode(name)), $(libs...)))
 	
+	# NOTE: about quote block:
+	#   1. inner @eval the function type in module scope in case it defines new types
+	#   2. outer @eval bind the function pointer and "bake" it into the function
 	return quote
-		@eval begin
-			$(map(last, deps)...)
-			$(name)($(argNames...)) = $(Expr(:$, func))($(argNames...))
-		end
+		@eval $(name)($(argNames...)) = $(Expr(:$, :(
+			(@eval CBinding.Cfunction{$(sig...)})($(QuoteNode(name)), $(libs...))
+		)))($(argNames...))
 	end
 end
 
-function _cextern_variable(mod::Module, deps::Union{Vector{Pair{Symbol, Expr}}, Nothing}, unused::Nothing, varName::Symbol, varType::Union{Symbol, Expr}, libs::Union{Symbol, Expr}...)
-	isnothing(deps) || error("Usage of @cextern cannot be nested")
-	deps = Pair{Symbol, Expr}[]
+function _cextern_variable(mod::Module, unused::Nothing, varName::Symbol, varType::Union{Symbol, Expr}, libs::Union{Symbol, Expr, Clibrary}...)
+	libs = map(l -> l isa Clibrary ? l : Expr(:$, esc(l)), libs)
 	
-	var = :(CBinding.Cglobal{$(varType)}($(QuoteNode(varName)), $(libs...)))
-	
+	# NOTE: @eval the variable type in module scope in case it defines new types and so it can be const
 	return quote
-		@eval begin
-			$(map(last, deps)...)
-			const $(varName) = $(var)
-		end
+		@eval const $(varName) = CBinding.Cglobal{$(varType)}($(QuoteNode(varName)), $(libs...))
 	end
 end
 
