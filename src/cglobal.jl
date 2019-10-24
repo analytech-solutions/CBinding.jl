a295de2a91ce9a2066f61e39d430ebe272aaf63e
Keith Rutkowski <keith@analytech-solutions.com>
2019-10-24 15:09:33 -0400

    Rework of Cglobal/Cglobalconst into one type

--- src/cglobal.jl
+++ src/cglobal.jl
@@ -1,28 +1,12 @@
 
 
-mutable struct Cglobalconst{T}
+struct Cglobal{T}
 	handle::Ptr{T}
 	
-	Cglobalconst{T}(lib::Clibrary, sym::Symbol) where {T} = new{T}(reinterpret(Ptr{T}, Libdl.dlsym(lib.handle, sym)))
-	Cglobalconst{T}() where {T} = new{T}(Ptr{T}(C_NULL))
+	Cglobal{T}(sym::Symbol, libs::Clibrary...) where {T} = new{T}(reinterpret(Ptr{T}, _dlsym(sym, libs...)))
 end
-mutable struct Cglobal{T}
-	handle::Ptr{T}
-	
-	Cglobal{T}(lib::Clibrary, sym::Symbol) where {T} = new{T}(reinterpret(Ptr{T}, Libdl.dlsym(lib.handle, sym)))
-	Cglobal{T}() where {T} = new{T}(Ptr{T}(C_NULL))
-end
-Base.getindex(g::Union{Cglobalconst{T}, Cglobal{T}}) where {T} = unsafe_load(g.handle)
+Cglobal{T}(lib::Clibrary, sym::Symbol) where {T} = Cglobal{T}(sym, lib)
+
+Base.getindex(g::Cglobal{T}) where {T} = unsafe_load(g.handle)
 Base.setindex!(g::Cglobal{T}, val::T) where {T} = unsafe_store!(g.handle, val)
 
-
-function bind(g::Union{Cglobalconst{T}, Cglobal{T}}, sym, libs::Clibrary...) where {T}
-		for (ind, lib) in enumerate(libs)
-			handle = Libdl.dlsym(lib.handle, sym, throw_error = ind == length(libs))
-			if !isnothing(handle)
-				g.handle = reinterpret(Ptr{T}, handle)
-				break
-			end
-		end
-		return nothing
-end
