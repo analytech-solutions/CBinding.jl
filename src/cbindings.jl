ebbe44c5f351dab08e19dca4cedd03f3b7441401
Keith Rutkowski <keith@analytech-solutions.com>
2019-12-31 08:17:03 -0500

    Switch @cbindings and @cextern to be statically compilable

--- src/cbindings.jl
+++ src/cbindings.jl
@@ -10,53 +10,35 @@ function _dlsym(sym::Symbol, libs::Clibrary...)
 end
 
 
-
 macro cbindings(exprs...) return _cbindings(__module__, exprs...) end
 
-function _cbindings(mod::Module, exprs...)
-	length(exprs) >= 1 || error("Expected @cbindings to have (optional) library expressions followed by a block of @cextern statements")
-	
-	syms = []
-	libs = map(enumerate(exprs[1:end-1])) do (ind, expr)
-		sym = gensym("lib")
-		push!(syms, Expr(:$, sym))
-		return quote
-			$(sym) = $(esc(expr))
-		end
-	end
-	
-	function addlibs(e)
+function _cbindings(mod::Module, block::Expr)
+	return _cbindings(mod, "", block)
+end
+
+function _cbindings(mod::Module, lib::String, block::Expr)
+	function _addlibs(e)
 		if Base.is_expr(e, :macrocall) && length(e.args) >= 1 && e.args[1] in _externExprs
-			append!(e.args, syms)
+			push!(e.args, lib)
 		elseif e isa Expr
 			for expr in e.args
-				addlibs(expr)
+				_addlibs(expr)
 			end
 		end
 	end
-	block = Base.is_expr(exprs[end], :block) ? exprs[end] : Expr(:block, exprs[end])
-	addlibs(block)
+	_addlibs(block)
 	
-	return quote
-		let
-			$(libs...)
-			@eval $(block)
-			nothing
-		end
-	end
+	return esc(block)
 end
-
 
 
 macro cextern(exprs...) return _cextern(__module__, exprs...) end
 
-function _cextern(mod::Module, expr::Expr, libs::Union{Symbol, Expr, Clibrary}...)
-	return _cextern(mod, nothing, expr, libs)
+function _cextern(mod::Module, expr::Expr, lib::String)
+	return _cextern(mod, nothing, expr, lib)
 end
 
-function _cextern(mod::Module, conv::Union{Symbol, Nothing}, expr::Expr, libs::Union{NTuple{N, Clibrary}, NTuple{N, Union{Symbol, Expr}}} where {N})
-	isempty(libs) && error("Usage of @cextern requires at least 1 library to be provided")
-	
+function _cextern(mod::Module, conv::Union{Symbol, Nothing}, expr::Expr, lib::String)
 	# allow for exposing alternative binding names in Julia with `const exposed = ...`
 	destName = nothing
 	if Base.is_expr(expr, :const, 1) && Base.is_expr(expr.args[1], :(=), 2) && expr.args[1].args[1] isa Symbol
@@ -66,10 +48,10 @@ function _cextern(mod::Module, conv::Union{Symbol, Nothing}, expr::Expr, libs::U
 	
 	Base.is_expr(expr, :(::), 2) || error("Expected @cextern to have a function signature, `func(arg1::Arg1Type, ..., argN::ArgNType)::ReturnType`, or a global variable, `variable::VarType`, expression, but found `$(sig)`")
 	(externName, externType) = expr.args
-	return (externName isa Symbol ? _cextern_variable : _cextern_function)(mod, conv, destName, externName, externType, eltype(libs) <: Clibrary ? libs : map(esc, libs))
+	return (externName isa Symbol ? _cextern_variable : _cextern_function)(mod, conv, destName, externName, externType, lib)
 end
 
-function _cextern_function(mod::Module, conv::Union{Symbol, Nothing}, destName::Union{Symbol, Nothing}, func::Expr, retType::Union{Symbol, Expr}, libs::Union{NTuple{N, Clibrary}, NTuple{N, Expr}} where {N})
+function _cextern_function(mod::Module, conv::Union{Symbol, Nothing}, destName::Union{Symbol, Nothing}, func::Expr, retType::Union{Symbol, Expr}, lib::String)
 	isnothing(conv) || (startswith(String(conv), "__") && endswith(String(conv), "__") && length(String(conv)) > 4) || error("Expected @cextern to have a calling convention specified as `__CONVENTION__`, such as `__cdecl__` or `__stdcall__`, but got `$(conv)`")
 	conv = isnothing(conv) ? conv : Symbol(String(conv)[3:end-2])
 	
@@ -91,30 +73,26 @@ function _cextern_function(mod::Module, conv::Union{Symbol, Nothing}, destName::
 	
 	sig = (
 		retType,
-		:(Tuple{$(argTypes...)}),
-		(isnothing(conv) ? () : (:(Cconvention{$(QuoteNode(conv))}),))...,
+		:($(@__MODULE__).Tuple{$(argTypes...)}),
+		(isnothing(conv) ? () : (:($(@__MODULE__).Cconvention{$(QuoteNode(conv))}),))...,
 	)
 	
 	destName = isnothing(destName) ? name : destName
-	
-	# NOTE: about quote block:
-	#   1. inner @eval the function type in module scope in case it defines new types
-	#   2. outer @eval bind the function pointer and "bake" it into the function
-	return quote
-		@eval $(destName)($(argNames...)) = $(Expr(:$, :(
-			(@eval $(@__MODULE__).Cfunction{$(sig...)})($(QuoteNode(name)), $(libs...))
+	return esc(quote
+		$(@__MODULE__).@eval $(destName)($(argNames...)) = $(Expr(:$, :(
+			$(@__MODULE__).DeferredPtr{
+				$(@__MODULE__).Cfunction{$(sig...)},
+				$(@__MODULE__).Val{$(QuoteNode(name))},
+				$(@__MODULE__).Val{$(QuoteNode(Symbol(lib)))}
+			}()
 		)))($(argNames...))
-	end
+	end)
 end
 
-function _cextern_variable(mod::Module, unused::Nothing, destName::Union{Symbol, Nothing}, varName::Symbol, varType::Union{Symbol, Expr}, libs::Union{NTuple{N, Clibrary}, NTuple{N, Expr}} where {N})
+function _cextern_variable(mod::Module, unused::Nothing, destName::Union{Symbol, Nothing}, varName::Symbol, varType::Union{Symbol, Expr}, lib::String)
 	destName = isnothing(destName) ? varName : destName
-	
-	# NOTE: @eval the variable type in module scope in case it defines new types and so it can be const
-	return quote
-		@eval const $(destName) = $(Expr(:$, :(
-			(@eval $(@__MODULE__).Cglobal{$(varType)})($(QuoteNode(varName)), $(libs...))
-		)))
-	end
+	return esc(quote
+		$(@__MODULE__).@warn "Cannot bind to global variable `$($(name))`, not yet implemented"
+	end)
 end
 
