9f87bea9e36cfa1c46c6d6224b29668587f9ae7f
Keith Rutkowski <keith@analytech-solutions.com>
2021-03-18 12:37:45 -0400

    Improve String support for Carray

--- src/arrays.jl
+++ src/arrays.jl
@@ -30,6 +30,11 @@ Base.length(::Type{CA}) where {T, N, CA<:Carrays{T, N}} = N
 Base.eltype(::Type{CA}) where {T, N, CA<:Carrays{T, N}} = T
 Base.convert(::Type{CA}, t::Tuple) where {CA<:Carray} = CA(t...)
 
+Base.convert(::Type{CA}, str::String) where {T<:Union{Int8, UInt8, Cconst{Int8}, Cconst{UInt8}}, CA<:Carray{T}} = CA(str...)
+Base.String(ca::Carray{T}) where {T<:Union{Int8, UInt8, Cconst{Int8}, Cconst{UInt8}}} = String(map(t -> reinterpret(UInt8, convert(unqualifiedtype(T), t)), collect(ca)))
+Base.show(io::IO, ca::Carray{T}) where {T<:Union{Int8, UInt8, Cconst{Int8}, Cconst{UInt8}}} = show(io, String(ca))
+
+
 Base.firstindex(ca::CA) where {CA<:Carrays} = 1
 Base.lastindex(ca::CA) where {CA<:Carrays} = length(ca)
 
